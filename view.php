ddb405d9a3f9345239b7257fc7f2fa368eb77f00
leedayeonggg <leedayeonggg@gmail.com>
2025-02-03 04:13:45 +0900

    view페이지작성

---
+++ view.php
@@ -0,0 +1,36 @@
+<?php
+  require_once('config.php');
+
+  // GET방식으로 넘어옴
+  $num = $_GET['idx'];
+
+  $sql = "SELECT * FROM board WHERE idx = {$num}";
+  $result = mysqli_query($mysqli, $sql);
+
+  print_r(mysqli_fetch_object($result))
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>View - boardTest</title>
+</head>
+<body>
+  <?php
+  if($row = mysqli_fetch_row($result)){
+  ?>
+  <h1>글 상세</h1>
+  <h2>글쓴이 : <?= $row->username; ?></h2>
+  <h3>글 내용</h3>
+  <div>
+  <?= $row->usermsg; ?>
+  </div>
+  <?php
+  } else {
+    echo "<script>alert('해당글이 없습니다'); location.href='index.php';</script>";
+  }
+  ?>
+</body>
+</html>

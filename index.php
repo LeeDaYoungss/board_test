b530376b3d11864c2d6690acc955c10cb3da87bd
leedayeonggg <leedayeonggg@gmail.com>
2025-02-03 03:45:00 +0900

    리스트출력

--- index.php
+++ index.php
@@ -3,6 +3,43 @@
   // 없으면 안되는 파일 가져오기 (DB연결 파일)
   require_once('config.php');
 
+  $sql = "SELECT * FROM board";
+  $result = mysqli_query($mysqli, $sql);
+
+  /*
+  print_r(mysqli_fetch_row($result));
+  // Array ( [0] => 9 [1] => 홍길동 [2] => 안녕하세요 )
+  */
+
+  /* fetch associative array 
+  while ($row = mysqli_fetch_row($result)) {
+    printf($row[1]);
+  }
+  // 홍길동홍길동hong이도령hong
+  */
+
+
+  /*
+  while ($row = mysqli_fetch_assoc($result)) {
+    printf($row["username"]);
+  }
+  */
+
+  /*
+  while ($row = mysqli_fetch_array($result)) {
+    printf($row[0]);
+    printf($row["username"]);
+  }
+  */
+
+  // 리스트가 나와야 함
+
+  $list = '';
+
+  while ($row = mysqli_fetch_object($result)) {
+    // printf($row->username);
+    $list = $list."<li><a href=''>{$row->username}</a></li>";
+  }
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +52,11 @@
 <body>
   <h1>BoardTest</h1>
   <ul>
-    <li><a href="">글 제목1</a><a href="">수정</a><a href="">삭제</a></li>
-    <li><a href="">글 제목1</a><a href="">수정</a><a href="">삭제</a></li>
-    <li><a href="">글 제목1</a><a href="">수정</a><a href="">삭제</a></li>
+    <?= $list; ?>
+<!--     
+    <li><a href="">글 제목1</a><a href="">수정</a><a href="">삭제</a></li> 
+    -->
+
   </ul>
   <p>
     <a href="write.php">글쓰기</a>
--- insert.php
+++ insert.php
@@ -6,5 +6,13 @@
 
   $sql = "INSERT INTO board (username,usermsg) VALUES('$username', '$usermsg')";
 
-  mysqli_query($mysqli, $sql);
+  // 실행 문법
+  $result = mysqli_query($mysqli, $sql);
+
+  if($result == false){
+    echo "<script>alert('저장 실패'); history.back();</script>";
+    error_log(mysqli_error($mysqli));
+  }else{
+    echo "<script>alert('저장 성공'); location.href='./index.php';</script>";
+  }
 ?>
